31e66faabfdab2699128014694dea6772d6cb65a
amanatid <116643953+amanatid@users.noreply.github.com>
2023-07-17 14:58:06 +0300

    Update base.py

--- base.py
+++ base.py
@@ -60,7 +60,7 @@ class ArxivReader_mod(BaseReader):
             query=search_query,
             id_list=[],
             max_results=max_results,
-            sort_by=arxiv.SortCriterion.Relevance,
+            sort_by=sort_criterion,
         )
 
         search_results = list(arxiv_search.results())
