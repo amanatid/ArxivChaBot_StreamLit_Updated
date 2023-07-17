0eef1a8d948066bfbade29d18c8e3405e5167611
amanatid <116643953+amanatid@users.noreply.github.com>
2023-07-17 13:08:44 +0300

    Update Streamlit_ChatBot_SideBar.py

--- Streamlit_ChatBot_SideBar.py
+++ Streamlit_ChatBot_SideBar.py
@@ -59,7 +59,7 @@ dummy = st.radio(
     ('Relevance', 'LastUpdated', 'SubmittedDate'))
 
 
-print(query,max_query)
+
 
 if query and max_query:
     if dummy == 'Relevance':
@@ -70,7 +70,7 @@ if query and max_query:
 
     if dummy == "SubmittedDate":
         search_query_int = 2
-    print(query,max_query,search_query_int)
+    
     try:
         # load the reader
         loader = ArxivReader_mod()
